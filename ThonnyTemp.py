ba66980901316ecf57eb5e30506b93c496b73366
Adam Larson <144720912+adamlarsonn@users.noreply.github.com>
2023-11-14 13:41:46 -0500

    Update ThonnyTemp.py

--- ThonnyTemp.py
+++ ThonnyTemp.py
@@ -1,10 +1,12 @@
 import serial
-
+# imports serial to read in ACM0 file containing arduino code 
 if __name__=='__main__':
     ser = serial.Serial('/dev/tty/ACM0', 9600, timeout=1)
     ser.flush()
 
     while True:
-        # if ser.in_waiting > 0:
+        # Reads in and prints voltage, celsius, and fahrenheit
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
+
+# if ser.in_waiting > 0:
